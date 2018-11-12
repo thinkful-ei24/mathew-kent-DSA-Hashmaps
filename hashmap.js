07c6b6f0fcb81a91b07829eabd4dee3cc0530caf
Kent Tokunaga <kenttokunaga@gmail.com>
2018-11-12 14:18:48 -0500

    Finish palindrome

--- hashmap.js
+++ hashmap.js
@@ -44,10 +44,10 @@ class HashMap {
     const hash = HashMap._hashString(key);
     const start = hash % this._capacity; // 3 -> 11
 
-    for(let i=start; i<start + this._capacity; i++) {
+    for (let i=start; i<start + this._capacity; i++) {
       const index = i % this._capacity;
       const slot = this._slots[index];
-      if (slot === undefined || slot.key == key && !slot._deleted ) {
+      if (slot === undefined || slot.key == key && !slot._deleted) {
         return index;
       }
     }
@@ -90,17 +90,46 @@ HashMap.MAX_LOAD_RATIO = 0.9;
 HashMap.SIZE_RATIO = 3;
 
 const lor = new HashMap();
-lor.set('Hobbit', "Bilbo");
-lor.set('Hobbit', "Frodo");
-lor.set('Wizard', "Gandolf");
-lor.set('Human',"Aragon");
-lor.set('Elf',"Legolas");
-lor.set('Maiar',"The Necromancer");
-lor.set('Maiar',"Sauron");
-lor.set('RingBearer',"Gollum");
-lor.set('LadyOfLight',"Galadriel");
-lor.set('HalfElven',"Arwen");
-lor.set('Ent',"Treebeard");
+lor.set('Hobbit', 'Bilbo');
+lor.set('Hobbit', 'Frodo');
+lor.set('Wizard', 'Gandolf');
+lor.set('Human', 'Aragon');
+lor.set('Elf', 'Legolas');
+lor.set('Maiar', 'The Necromancer');
+lor.set('Maiar', 'Sauron');
+lor.set('RingBearer', 'Gollum');
+lor.set('LadyOfLight', 'Galadriel');
+lor.set('HalfElven', 'Arwen');
+lor.set('Ent', 'Treebeard');
 // console.log(lor.get('Hobbit'))
-serialize(lor)
-console.log(lor.get('Maiar'))
+serialize(lor);
+console.log(lor.get('Maiar'));
+
+// input: String, example: acecarr
+// output: Bool, example, true
+// Loop through the string one character at a time
+// keep track of how many times each letter is used
+// Look at each char in the hash map
+// if there are 2 or more characters with odd counts, return false
+// otherwise return true
+
+function palindrome(str) {
+  const charMap = new HashMap();
+  let odds = 0;
+  let count;
+  for (let i=0; i<str.length; i++) {
+    try {
+      count = charMap.get(str[i]);
+    } catch (e) {
+      count = 0;
+    }
+    charMap.set(str[i], count + 1);
+  }
+
+  for (let i=0; i<str.length; i++) {
+    count = charMap.get(str[i]);
+    if (count % 2) odds++;
+    if (odds > 1) return false;
+  }
+  return true;
+}
