f86e1a6d8f3056a7822523745069ac2bcd17d49d
Matt Williamson <mathewbwilliamson@gmail.com>
2018-11-12 12:28:08 -0500

    The HashMap is done

--- hashmap.js
+++ hashmap.js
@@ -3,6 +3,69 @@ class HashMap {
     this.length = 0;
     this._slots = [];
     this._capacity = initialCapacity;
+    this._deleted = 0;
+  }
+
+  get(key) {
+    const index = this._findSlot(key);
+    if (this._slots[index] === undefined) {
+      throw new Error('Key Error');
+    }
+    return this._slots[index].value;
+  }
+
+  set(key, value) {
+    const loadRatio = (this.length + this._deleted + 1) / this._capacity;
+    if (loadRatio > HashMap.MAX_LOAD_RATIO) {
+      this._resize(this._capacity * HashMap.SIZE_RATIO);
+    }
+    const index = this._findSlot(key);
+    this._slots[index] = {
+      key,
+      value,
+      deleted: false
+    };
+    this.length++;
+  }
+
+  remove(key) {
+    const index = this._findSlot(key);
+    const slot = this._slots[index];
+    if (slot === undefined) {
+      throw new Error('Key error');
+    }
+
+    slot.deleted = true;
+    this.length--;
+    this._deleted++;
+  }
+
+  _findSlot(key) {
+    const hash = HashMap._hashString(key);
+    const start = hash % this._capacity; // 3 -> 11
+
+    for(let i=start; i<start + this._capacity; i++) {
+      const index = i % this._capacity;
+      const slot = this._slots[index];
+      if (slot === undefined || slot.key == key && !slot._deleted ) {
+        return index;
+      }
+    }
+  }
+
+  _resize(size) {
+    const oldSlots = this._slots;
+    this._capacity = size;
+
+    this.length = 0;
+    this._deleted = 0;
+    this._slots = [];
+
+    for (const slot of oldSlots) {
+      if (slot !== undefined && !slot.deleted) {
+        this.set(slot.key, slot.value);
+      }
+    }
   }
 
   static _hashString(string) {
@@ -13,34 +76,31 @@ class HashMap {
     }
     return hash >>> 0;
   }
+}
 
-  set(key, value) {
-    const loadRatio = (this.length + 1) / this._capacity;
-    if (loadRatio > HashMap.MAX_LOAD_RATIO) {
-      this._resize(this._capacity * HashMap.SIZE_RATIO);
-    }
-
-    const index = this._findSlot(key);
-    this._slots[index] = {
-      key,
-      value
-    };
-    this.length++;
-  }
-
-  _findSlot(key) {
-    const hash = HashMap._hashString(key);
-    const start = hash % this._capacity;
-
-    for (let i=start; i<start + this._capacity; i++) {
-      const index = i % this._capacity;
-      const slot = this._slots[index];
-      if (slot === undefined || slot.key == key) {
-        return index;
-      }
+function serialize(hashMap) {
+  for (const slot of hashMap._slots) {
+    if (slot !== undefined && !slot.deleted) {
+      console.log(hashMap.get(slot.key, slot.value));
     }
   }
 }
 
 HashMap.MAX_LOAD_RATIO = 0.9;
 HashMap.SIZE_RATIO = 3;
+
+const lor = new HashMap();
+lor.set('Hobbit', "Bilbo");
+lor.set('Hobbit', "Frodo");
+lor.set('Wizard', "Gandolf");
+lor.set('Human',"Aragon");
+lor.set('Elf',"Legolas");
+lor.set('Maiar',"The Necromancer");
+lor.set('Maiar',"Sauron");
+lor.set('RingBearer',"Gollum");
+lor.set('LadyOfLight',"Galadriel");
+lor.set('HalfElven',"Arwen");
+lor.set('Ent',"Treebeard");
+// console.log(lor.get('Hobbit'))
+serialize(lor)
+console.log(lor.get('Maiar'))
